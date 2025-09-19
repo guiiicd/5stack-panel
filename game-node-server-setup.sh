777cb91c1cd1694c3e454b08ab1aa0009a3d4dc3
Luke Policinski <Luke@LukePOLO.com>
2025-09-19 12:08:33 -0400

    chore: add more docs when installing to make sure tailscale is setup properly

--- game-node-server-setup.sh
+++ game-node-server-setup.sh
@@ -9,20 +9,26 @@ curl -sfL https://tailscale.com/install.sh | sh
 echo -e "\033[1;31mSetup access control for the fivestack tag using the (10.42.0.0/16) subnet https://login.tailscale.com/admin/acls/visual/general-access-rules \033[0m"
 cat tag-setup-ascii
 
-echo "Generate and enter your Tailscale auth key: https://login.tailscale.com/admin/settings/keys, make sure to select the \"Pre Approved\" option"
+echo -e "\033[1;31mSetup auto approver routes for the fivestack tag using the (10.42.0.0/16) subnet https://login.tailscale.com/admin/acls/visual/general-access-rules \033[0m"
+cat auto-approvers-ascii
 
-echo -e "\033[1;36mEnter your Tailscale auth key:\033[0m"
+echo "Generate and enter your Tailscale auth key : https://login.tailscale.com/admin/settings/keys :"
+
+echo -e "\033[1;31mMake sure to select the \"Pre Approved\" option \033[0m"
+cat pre-approved-ascii
+
+echo -e "\033[1;36mEnter your Tailscale auth key :\033[0m"
 read TAILSCALE_AUTH_KEY
 
 while [ -z "$TAILSCALE_AUTH_KEY" ]; do
-    echo "Tailscale auth key cannot be empty. Please enter your Tailscale auth key:"
+    echo "Tailscale auth key cannot be empty. Please enter your Tailscale auth key :"
     read TAILSCALE_AUTH_KEY
 done
 
 curl -sfL https://get.k3s.io | sh -s - --disable=traefik --vpn-auth="name=tailscale,joinKey=${TAILSCALE_AUTH_KEY}";
 
 
-echo -e "\033[1;36mEnter your Tailscale network name (e.g. example.ts.net) from https://login.tailscale.com/admin/dns:\033[0m"
+echo -e "\033[1;36mEnter your Tailscale network name (e.g. example.ts.net) from https://login.tailscale.com/admin/dns :\033[0m"
 read TAILSCALE_NET_NAME
 while [ -z "$TAILSCALE_NET_NAME" ]; do
     echo "Tailscale network name cannot be empty. Please enter your Tailscale network name (e.g. example.ts.net):"
@@ -31,10 +37,10 @@ done
 
 update_env_var "overlays/config/api-config.env" "TAILSCALE_NET_NAME" "$TAILSCALE_NET_NAME"
 
-echo -e "\033[1;31mCreate an OAuth Client with the Auth Keys (\`auth_keys\`) scope with write access and a tag of fivestack from https://login.tailscale.com/admin/settings/oauth\033[0m"
+echo -e "\033[1;31mCreate an OAuth Client with the Auth Keys (\`auth_keys\`) scope with write access and a tag of fivestack from https://login.tailscale.com/admin/settings/oauth :\033[0m"
 cat auth-key-ascii
 
-echo -e "\033[1;36mEnter your Secret Key from the step above:\033[0m"
+echo -e "\033[1;36mEnter your Secret Key from the step above :\033[0m"
 read TAILSCALE_SECRET_ID
 while [ -z "$TAILSCALE_SECRET_ID" ]; do
     echo "Tailscale secret key cannot be empty. Please enter your Tailscale secret key:"
@@ -43,7 +49,7 @@ done
 
 update_env_var "overlays/local-secrets/tailscale-secrets.env" "TAILSCALE_SECRET_ID" "$TAILSCALE_SECRET_ID"
 
-echo -e "\033[1;36mEnter the Client ID from the Step Above:\033[0m"   
+echo -e "\033[1;36mEnter the Client ID from the Step Above :\033[0m"   
 read TAILSCALE_CLIENT_ID
 while [ -z "$TAILSCALE_CLIENT_ID" ]; do
     echo "Tailscale client ID cannot be empty. Please enter your Tailscale client ID:"
@@ -52,7 +58,7 @@ done
 
 update_env_var "overlays/config/api-config.env" "TAILSCALE_CLIENT_ID" "$TAILSCALE_CLIENT_ID"
 
-echo -e "\033[1;36mOn the tailscale dashboard you should see your node come online, once it does enter the IP Address of the node:\033[0m"
+echo -e "\033[1;36mOn the tailscale dashboard you should see your node come online, once it does enter the IP Address of the node :\033[0m"
 read TAILSCALE_NODE_IP
 while [ -z "$TAILSCALE_NODE_IP" ]; do
     echo "Tailscale node IP cannot be empty. Please enter your Tailscale node IP:"
